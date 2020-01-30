0b98f7c9ee1c82caaa0009c433d2d7c18d6d45e5
EdwinBoersema <edwinboersema@gmail.com>
2020-01-30 13:54:05 +0100

    Add session exercise.

---
+++ intermediate/sessions/page4.php
@@ -0,0 +1,25 @@
+<?php 
+    session_start();
+
+    // unset($_SESSION['name']);
+
+    session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Sessions</title>
+</head>
+
+<body>
+<a href="page1.php">Page 1</a>
+<a href="page2.php">Page 2</a>
+<a href="page3.php">Page 3</a>
+</body>
+
+</html>
