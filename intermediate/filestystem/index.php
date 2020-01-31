9b74facf062c6affd7000f33fe19503ba670f953
EdwinBoersema <edwinboersema@gmail.com>
2020-01-31 10:16:06 +0100

    Add filesystem exercise.

---
+++ intermediate/filestystem/index.php
@@ -0,0 +1,68 @@
+<?php 
+
+    $path = '/dir1/examplefile.php';
+    $file = 'file1.txt';
+/*
+    // Return filename
+    echo basename($path);
+
+    // Return filename without extension
+    echo basename($path, 'php');
+
+    // Return directory name from path
+    echo dirname($path);
+
+    // Check if file exists
+    echo file_exists($file);
+
+    // Get absolute path
+    echo realpath($file);
+
+    // Check to see if file
+    echo is_file($file);
+
+    // Check if file is writable
+    echo is_writeable($file);
+
+    // Get filesize
+    echo filesize($file);
+
+    // Create a directory
+    mkdir('testing');
+
+    // Remove a directory
+    rmdir('testing');
+
+    // Copy file
+    echo copy('file1.txt', 'file2.txt');
+
+    // Rename file
+    rename('file2.txt', 'myfile.txt');
+
+    // Delete file
+    unlink('myfile.txt');
+
+    // Write from file to string
+    echo file_get_contents($file);
+
+    // Write string to file
+    echo file_put_contents($file, "Hello everyone");
+
+    // Append to file
+    $current = file_get_contents($file);
+
+    $current .= ' Goodbye World';
+
+    file_put_contents($file, $current);
+
+    // Open file for Reading
+    $handle = fopen($file, 'r');
+    $data = fread($handle, filesize($file));
+    echo $data;
+    */
+    
+    // Open file for Writing
+    // $handle = fopen('file2.txt', 'w');
+
+    // $text = 'Johnathan';
+    // fwrite($handle, $text);
